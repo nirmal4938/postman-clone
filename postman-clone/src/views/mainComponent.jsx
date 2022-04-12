57f513a146869a5d019ef4eee2937da1d9acc220
Nirmal Kumar <nirmal.kumar@pragmaapps.com>
2022-04-13 03:05:25 +0530

    initial commit

---
+++ postman-clone/src/views/mainComponent.jsx
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+import Navbar from './navbar';
+
+class MainComponent extends Component {
+    state = {};
+
+
+
+
+    render(){
+
+        return(<React.Fragment>
+          <Navbar/>
+        </React.Fragment>);
+    }
+};
+export default MainComponent;
---
+++ postman-clone/src/views/navbar.jsx
@@ -0,0 +1,22 @@
+import React,{Component} from "react";
+
+class Navbar extends Component {
+    state = {};
+
+
+
+
+
+    render()
+    {
+        return(
+   <React.Fragment>
+    <nav class="navbar navbar-light " style={{ backgroundColor: '#ffaa80'}}>
+    <h5>Postman</h5>
+    </nav>
+            </React.Fragment>
+        );
+    }
+
+}
+export default Navbar;
